89b63699fbb5a0ef18cc57c27ae2a0653cd0e509
Maria Chiara Lischi <102853281+MariaChiaraLischi@users.noreply.github.com>
2025-09-13 20:13:50 +0200

    ready

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-
+// tx trweil https://www.reddit.com/r/nextjs/comments/1ixfypv/nextjs_on_github_pages/
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
@@ -7,9 +7,9 @@ const nextConfig: NextConfig = {
     unoptimized: true
   },
   // // GitHub Pages deployment
-  // basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
-  distDir: 'out',
+  basePath: process.env.NODE_ENV === 'production' ? '/mariachiaralischi.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mariachiaralischi.github.io' : '',
+  
 };
 
 export default nextConfig;
