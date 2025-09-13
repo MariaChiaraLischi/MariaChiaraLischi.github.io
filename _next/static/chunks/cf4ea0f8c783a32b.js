c1ebb8d97a78827043efb05ed1eba610e7c837b7
Maria Chiara Lischi <102853281+MariaChiaraLischi@users.noreply.github.com>
2025-09-13 20:44:49 +0200

    Updates

--- _next/static/chunks/cf4ea0f8c783a32b.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/e09148e53bf3d264.js",
-  "static/chunks/turbopack-20896efd9b4e7ed0.js"
-])
--- _next/static/chunks/da1e472dae528b45.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/e09148e53bf3d264.js",
-  "static/chunks/turbopack-b1fc8573931e49be.js"
-])
